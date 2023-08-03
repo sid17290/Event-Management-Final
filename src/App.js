ab9045a9d14184e45aff1e34a4e46573f26c5311
rishabhjain2601 <rishabhjain2dtu@gmail.com>
2023-08-03 23:38:11 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Event_page from './pages/Event_page';
+
+function App() {
+  return (
+    <div className="App">
+      <Event_page/>
+    </div>
+  );
+}
+
+export default App;
