7147d3923c7c1943322815c3c52435ef508e8ea0
rishabhjain2601 <rishabhjain2dtu@gmail.com>
2023-08-04 01:06:03 +0530

    event page updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Society from './pages/Society';
+import Event from './pages/Event';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Society/>} />
+          <Route path="/Society" element={<Society/>} />
+          <Route path="/Event" element={<Event/>} />
         </Routes>
       </Router>
     </>
