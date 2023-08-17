9423241a2df94af1784a1817848279884d62d2fe
shashwat-j <shashwatjain_ec21a18_23@dtu.ac.in>
2023-08-17 15:36:25 +0530

    society.js mobile

--- src/pages/Society.js
+++ src/pages/Society.js
@@ -25,31 +25,31 @@ const Society = () => {
 
         <nav className='h-12 text-white'>Navbar</nav>
 
-        <main className="w-8/12 flex flex-col gap-7 items-center">
+        <main className="w-full md:w-8/12 flex flex-col gap-7 items-center">
             {/* banner */}
-            <div className='w-full h-36'>
-                <img src={society?.banner} className='object-cover w-full h-full rounded-2xl'/>
+            <div className='w-full h-20 md:h-36'>
+                <img src={society?.banner} className='object-cover w-full h-full md:rounded-2xl'/>
             </div>
 
             {/* title div */}
-            <div className='flex gap-7 w-full'>
+            <div className='flex gap-7 w-10/12 md:w-full'>
                 {/* icon */}
-                <div className='h-36 w-36 rounded-full bg-darkGray'>
+                <div className='h-20 w-20 md:h-36 md:w-36 rounded-full bg-darkGray'>
                     <img src={society?.logo} className='rounded-full h-full w-full object-cover'/>
                 </div>
                 {/* title etc*/}
-                <div className='flex flex-col w-1/3 gap-1'>
-                    <h1 className='text-white text-3xl font-bold'>{society?.name}</h1>
+                <div className='flex flex-col md:w-1/3 md:gap-1'>
+                    <h1 className='text-white text-xl font-medium md:text-3xl md:font-bold'>{society?.name}</h1>
                     <h3 className='text-lightestGray text-lg'>{society?.type}</h3>
                     <h3 className='text-lightestGray text-lg'>{society?.member_count} members</h3>
-                    <div className='flex gap-4 items-center'>
+                    <div className='flex gap-3 md:gap-4 items-center w-full'>
                         <span className='text-lightestGray text-lg'>Registrations</span>
                         <span className={`h-[6px] w-[6px] rounded-full ${society?.registration_is_open?'bg-green-500':'bg-red-600'}`}>.</span>
                         <span className='text-white text-lg'>{society?.registration_is_open?'Open':'Closed'}</span>
                     </div>
                 </div>
                 {/* socials */}
-                <div className='flex gap-3'>
+                <div className='hidden md:flex gap-3'>
                     <div className='cursor-pointer h-[28px] w-[28px]' onClick={()=>{window.open(society?.linkedin)}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                         <path d="M25.7495 0H2.2505C1.008 0 0 1.008 0 2.2505V25.7495C0 26.992 1.008 28 2.2505 28H25.7495C26.992 28 28 26.992 28 25.7495V2.2505C28 1.008 26.992 0 25.7495 0ZM25.7495 25.76C10.0765 25.7565 2.24 25.753 2.24 25.7495C2.2435 10.0765 2.247 2.24 2.2505 2.24C17.9235 2.2435 25.76 2.247 25.76 2.2505C25.7565 17.9235 25.753 25.76 25.7495 25.76ZM4.151 10.4965H8.3055V23.8595H4.151V10.4965ZM6.23 8.6695C7.5565 8.6695 8.638 7.5915 8.638 6.2615C8.638 5.94528 8.57572 5.63215 8.4547 5.34C8.33369 5.04785 8.15632 4.78239 7.93271 4.55879C7.70911 4.33518 7.44365 4.15781 7.1515 4.0368C6.85935 3.91578 6.54622 3.8535 6.23 3.8535C5.91378 3.8535 5.60065 3.91578 5.3085 4.0368C5.01635 4.15781 4.75089 4.33518 4.52729 4.55879C4.30368 4.78239 4.12631 5.04785 4.0053 5.34C3.88428 5.63215 3.822 5.94528 3.822 6.2615C3.8185 7.5915 4.8965 8.6695 6.23 8.6695ZM15.0605 17.248C15.0605 15.505 15.393 13.818 17.5525 13.818C19.6805 13.818 19.712 15.8095 19.712 17.36V23.8595H23.863V16.5305C23.863 12.9325 23.086 10.164 18.8825 10.164C16.863 10.164 15.5085 11.2735 14.952 12.3235H14.896V10.4965H10.9095V23.8595H15.0605V17.248Z" fill="white" fillOpacity="0.6"/>
@@ -67,7 +67,9 @@ const Society = () => {
             <div className={`bg-lightGray w-full rounded-2xl`}>
                 <div className='py-4 px-6'>
                     <h3 className='text-white pb-2'>About</h3>
-                    <p className='text-lightestGray'>{aboutIsExpanded? society?.about : society?.about?.substring(0,450)}<span className={`text-yellow-400 cursor-pointer ${society?.about?.length < 451? 'hidden' :  ''}`} onClick={()=>{setAboutIsExpanded((aboutIsExpanded)=>!aboutIsExpanded)}}>{aboutIsExpanded? ' ...less' : ' more...'}</span></p>
+                    {/*p with more text for dektop, p with less text for mobile*/}
+                    <p className='hidden md:block text-lightestGray'>{aboutIsExpanded? society?.about : society?.about?.substring(0,450)}<span className={`text-yellow-400 cursor-pointer ${society?.about?.length < 451? 'hidden' :  ''}`} onClick={()=>{setAboutIsExpanded((aboutIsExpanded)=>!aboutIsExpanded)}}>{aboutIsExpanded? ' ...less' : ' more...'}</span></p>
+                    <p className='md:hidden text-lightestGray'>{aboutIsExpanded? society?.about : society?.about?.substring(0,250)}<span className={`text-yellow-400 cursor-pointer ${society?.about?.length < 251? 'hidden' :  ''}`} onClick={()=>{setAboutIsExpanded((aboutIsExpanded)=>!aboutIsExpanded)}}>{aboutIsExpanded? ' ...less' : ' more...'}</span></p>
                 </div>
             </div>
 
@@ -76,14 +78,14 @@ const Society = () => {
 
             {/* recruitment process */}
             <div className='bg-lightGray w-full rounded-2xl'>
-                <div className='py-8 px-12'>
+                <div className='py-8 px-6 md:px-12'>
 
                     <div className='flex justify-between items-center'>
-                        <h3 className='text-white text-2xl font-semibold'>Recruitment Process</h3>
-                        <button className='bg-darkGray border border-white py-2 px-10 rounded-lg cursor-pointer text-white text-lg'>Register</button>
+                        <h3 className='text-white text-lg md:text-2xl font-medium md:font-semibold'>Recruitment Process</h3>
+                        <button className='bg-darkGray border border-white py-2 px-6 md:px-10 rounded-lg cursor-pointer text-white text-md md:text-lg'>Register</button>
                     </div>
 
-                    <div className='mt-5'>
+                    <div className='mt-5 ml-2 md:ml-0'>
                         {society?.recruitment_process?.map((step, index)=>{
                             return(
                                 <div className='relative h-full flex mt-[2px]' key={index}>
@@ -91,7 +93,7 @@ const Society = () => {
                                     <div className='absolute rounded-full bg-yellow-400 h-3 w-3 outline outline-offset-2 outline-yellow-400 outline-1 top-5 -left-1'></div>
                                     <div className='mx-5 mb-4'>
                                         <p className='text-yellow-400 text-lg font-mono'>Step {index+1}</p>
-                                        <p className='text-lightestGray'>{step}</p>
+                                        <p className='text-lightestGray text-sm md:text-md'>{step}</p>
                                     </div>
                                 </div>
                             )
